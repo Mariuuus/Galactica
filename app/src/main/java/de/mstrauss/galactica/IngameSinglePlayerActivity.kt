a13880fe4e3d2313b22d4d39fb51012d82583268
Strauss, Marius Christoph <mariuschristoph.strauss@student.hs-anhalt.de>
2026-02-17 10:42:19 +0100

    feat(ui): gamefield init

---
+++ app/src/main/java/de/mstrauss/galactica/IngameSinglePlayerActivity.kt
@@ -0,0 +1,20 @@
+package de.mstrauss.galactica
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class IngameSinglePlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ingame_single_player)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
