c670944a473617e17cb374b4e66da5b934c4488e
Mariuuus <63265366+Mariuuus@users.noreply.github.com>
2026-02-21 22:41:22 +0100

    feat(ui): add ui dynamic text ui elements

--- app/src/main/java/de/mstrauss/galactica/SinglePlayerActivity.kt
+++ app/src/main/java/de/mstrauss/galactica/SinglePlayerActivity.kt
@@ -2,16 +2,20 @@ package de.mstrauss.galactica
 
 import android.os.Bundle
 import android.widget.GridLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import de.mstrauss.galactica.game.Cell
 import de.mstrauss.galactica.game.Game
 
 class SinglePlayerActivity : AppCompatActivity() {
 
     lateinit var game: Game
 
+    lateinit var movesLeftTextView: TextView
+    lateinit var planetsFoundTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +27,7 @@ class SinglePlayerActivity : AppCompatActivity() {
             insets
         }
 
-        game = Game(context=this)
+        game = Game(context=this, onUIRefresh = { refreshUITextElements(it) })
 
         val grid = findViewById<GridLayout>(R.id.single_player_grid)
         grid.rowCount = game.gridRows
@@ -34,5 +38,14 @@ class SinglePlayerActivity : AppCompatActivity() {
                 grid.addView(game.field[row][col])
             }
         }
+
+        movesLeftTextView = findViewById<TextView>(R.id.moves_left_text_view)
+        planetsFoundTextView = findViewById<TextView>(R.id.planets_founds_text_view)
+    }
+
+
+    fun refreshUITextElements(cell: Cell) {
+        movesLeftTextView.text = getString(R.string.x_moves_left, game.movesLeft)
+        planetsFoundTextView.text = getString(R.string.y_von_z_planeten_gefunden, game.planetsFound, game.planetAmount)
     }
 }
